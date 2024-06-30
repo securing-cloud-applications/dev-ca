6a384d0625fd0d59955ec64b99fb9e3b552f036e
Adib Saikali <asaikali@gmail.com>
2024-06-30 14:45:02 +0200

    polish

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,16 +1,29 @@
 package com.example;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Enumeration;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello time is: " + LocalDateTime.now() +"\n";
+    public String hello(HttpServletRequest request) {
+        boolean isSecure = request.isSecure();
+        StringBuilder headers = new StringBuilder();
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = request.getHeader(headerName);
+            headers.append(headerName).append(": ").append(headerValue).append("\n");
+        }
+
+        return "Hello time is: " + LocalDateTime.now() + "\n" +
+                "Connection is secure: " + isSecure + "\n" +
+                "HTTP Headers:\n" + headers.toString();
     }
 }
